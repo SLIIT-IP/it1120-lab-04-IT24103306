dcd86a6404633359ba55cc8fea7eca54ce0bfe38
IT24103306 <it24103306@my.sliit.lk>
2024-08-16 10:21:14 +0530

    Add IT24103306Lab4Q1.java

---
+++ Q1/IT24103306Lab4Q1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+	public class IT24103306Lab4Q1{
+public static void main(String[]args){
+Scanner scanner=new Scanner(System.in);
+System.out.print("input number : ");
+int num=scanner.nextInt();
+if (num==0)
+ {
+System.out.println("The number is : zero");
+ }
+else if (num>0)
+ {
+System.out.println("The number is : positive");
+ }
+else
+ {
+System.out.println("The number is : Negative");
+ }
+	}
+}
+
+
+
+
+	
